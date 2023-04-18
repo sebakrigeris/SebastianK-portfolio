c0b54b533fc83067ff3119a156a4ed7caae7945a
Sebastian Krigeris <sebastiankri123@gmail.com>
2023-04-18 15:43:27 -0300

    deploy 2

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,7 +1,20 @@
 const abrirMenu = document.querySelector(".abrir-menu");
 const menu = document.querySelector(".menu-links");
+const enlaces = document.querySelectorAll('ul a');
 
+
+// ABRIR NAV
 
 abrirMenu.addEventListener( "click", () => {
     menu.classList.toggle("nav-visible");
+});
+
+
+// LA NAV SE CIERRA CUANDO DOY CLICK EN LOS ENLACES
+
+enlaces.forEach(enlace => {
+    enlace.addEventListener('click', () => {
+    const nav = document.querySelector('ul');
+    nav.classList.remove('nav-visible');
+    });
 });
